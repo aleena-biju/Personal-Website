aa37c7b489ae4f749130fa4f75c1d64cdbed3a30
Aleena Biju <aleenabmarachery@gmail.com>
2020-06-10 07:24:31 +0530

    js file modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@
   "use strict";
 
   // Nav Menu
-  $(document).on('click', '.nav-menu a, .mobile-nav a', function(e) {
+  /*$(document).on('click', '.nav-menu a, .mobile-nav a', function(e) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var hash = this.hash;
       var target = $(hash);
@@ -47,7 +47,7 @@
 
       }
     }
-  });
+  }); */
 
   // Activate/show sections on load with hash links
   if (window.location.hash) {
@@ -149,7 +149,7 @@
   });
   
   //scroll to top
- $(document).ready(function() {
+ /*$(document).ready(function() {
    
     $("#about").click(function(){
         $('html, body').animate({scrollTop:0}, 'slow');
@@ -171,7 +171,7 @@
       return false;
     });
 
-  }); 
+  }); */ 
 
  /* $('document').ready(function() {
   $('body').scrollTop(0);
@@ -182,5 +182,40 @@
 window.scrollTo(0,0);
 }); */
 
+
+// Smoth scroll on page hash links
+$('a[href*="#"]:not([href="#"])').on('click', function() {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+
+    var target = $(this.hash);
+    if (target.length) {
+      var top_space = 0;
+
+      if ($('#header').length) {
+        top_space = $('#header').outerHeight();
+
+        if (!$('#header').hasClass('header-fixed')) {
+          top_space = top_space - 630;
+        }
+      }
+
+      $('html, body').animate({
+        scrollTop: target.offset().top - top_space
+      }, 1500, 'easeInOutExpo');
+
+      if ($(this).parents('.nav-menu').length) {
+        $('.nav-menu .menu-active').removeClass('menu-active');
+        $(this).closest('li').addClass('menu-active');
+      }
+
+      if ($('body').hasClass('mobile-nav-active')) {
+        $('body').removeClass('mobile-nav-active');
+        $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+        $('#mobile-body-overly').fadeOut();
+      }
+      return false;
+    }
+  }
+});
 
 })(jQuery);
